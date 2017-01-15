1b47176df16dbcd51a07760563e7e07b534f9424
HouHangLei <13716878917@163.com>
2017-01-15 15:08:01 +0800

    1

--- AuctionBlock/ViewController.m
+++ AuctionBlock/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    // 
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     button.frame = CGRectMake(0, 100, 100, 100);
     button.backgroundColor = [UIColor blackColor];
@@ -29,7 +30,6 @@
         [alertView show];
     }];
     [self.view addSubview:button];
-    
     
     
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 200, 100, 100)];
